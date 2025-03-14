f91cab4eae0c74350f729c53ba042668fbad6f47
aan krisnawati <aankrisnawati8@gmail.com>
2025-03-15 02:07:26 +0700

    memberikan sepasi pada baris 15

--- Aan.py
+++ Aan.py
@@ -7,9 +7,13 @@ def hitung_rata2(daftar_nilai):
 
 def cetak_hasil(nama, daftar_nilai):
     hasil = hitung_rata2(daftar_nilai)
-    print("Mahasiswa: " + nama + ", Rata-rata nilai: " + str(hasil))
+    print(f"Mahasiswa: {nama}, Rata-rata nilai: {hasil}")
     daftar_nilai.sort()
-    print("Nilai Terendah:", daftar_nilai[0], "| Nilai Tertinggi:", daftar_nilai[-1])
-  
-nilai_siswa=[80, 75, 90, 85,100,60]
-cetak_hasil ("Andi" , nilai_siswa)
+    print(
+      f"Nilai Terendah: {daftar_nilai[0]} | "
+      f"Nilai Tertinggi: {daftar_nilai[-1]}"
+    )
+
+
+nilai_siswa = [80, 75, 90, 85, 100, 60]
+cetak_hasil("Andi", nilai_siswa)
