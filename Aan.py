71c1d04b3ba1df61a269cb122bd02aedac7df973
aan krisnawati <aankrisnawati8@gmail.com>
2025-03-15 00:33:59 +0700

    menambahkan dan menghilangkan spasi

--- Aan.py
+++ Aan.py
@@ -6,7 +6,7 @@ def hitung_rata2(daftar_nilai):
 
 
 def cetak_hasil(nama, daftar_nilai):
- hasil=hitung_rata2( daftar_nilai )
+ hasil = hitung_rata2(daftar_nilai)
  print("Mahasiswa: "+nama+", Rata-rata nilai:"+str( hasil ))
  daftar_nilai.sort() ;print ("Nilai Terendah:",daftar_nilai[0],"| Nilai Tertinggi:",daftar_nilai[-1] )
   
