61ac9e08974a22dd8dde664db551046f8e3f882e
aan krisnawati <aankrisnawati8@gmail.com>
2025-03-15 00:44:33 +0700

    memberikan 4 spasi pada baris 9

--- Aan.py
+++ Aan.py
@@ -6,9 +6,10 @@ def hitung_rata2(daftar_nilai):
 
 
 def cetak_hasil(nama, daftar_nilai):
- hasil = hitung_rata2(daftar_nilai)
- print("Mahasiswa: "+nama+", Rata-rata nilai:"+str( hasil ))
- daftar_nilai.sort() ;print ("Nilai Terendah:",daftar_nilai[0],"| Nilai Tertinggi:",daftar_nilai[-1] )
+    hasil = hitung_rata2(daftar_nilai)
+    print("Mahasiswa: " + nama + ", Rata-rata nilai: " + str( hasil ))
+    daftar_nilai.sort();
+    print ("Nilai Terendah:",daftar_nilai[0],"| Nilai Tertinggi:",daftar_nilai[-1] )
   
 nilai_siswa=[80, 75, 90, 85,100,60]
 cetak_hasil ("Andi" , nilai_siswa)
